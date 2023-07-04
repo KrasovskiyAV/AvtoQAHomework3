221188cc2c0bdcae65b417f63efc1cfe4a90a678
Красовский АВ <AV.Krasovskiy@ya.ru>
2023-07-04 20:22:41 +0700

    fixed

--- src/test/java/ru/netology/web/OrderTest.java
+++ src/test/java/ru/netology/web/OrderTest.java
@@ -4,12 +4,13 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class OrderTest {
     @Test
-    void shouldTest() throws InterruptedException {
+    void shouldTest() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Красовский Алексей");
@@ -17,67 +18,61 @@ class OrderTest {
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-        Thread.sleep(5000);
     }
 
     @Test
-    void shouldNotEndApplicationIfCheckboxEmpty() throws InterruptedException {
+    void shouldNotEndApplicationIfCheckboxEmpty() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Красовский Алексей");
         form.$("[data-test-id=phone] input").setValue("+79139999999");
-        $("[data-test-id=agreement]");
         form.$(".button").click();
-        $("[data-test-id=agreement] input_invalid");
-        Thread.sleep(5000);
+        $("[data-test-id=agreement].input_invalid").shouldBe(visible);
     }
 
     @Test
-    void shouldShowErrorIfNameEmptyTest() throws InterruptedException {
+    void shouldShowErrorIfNameEmptyTest() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("");
         form.$("[data-test-id=phone] input").setValue("+79139999999");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-        Thread.sleep(3000);
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldShowErrorIfPhoneEmptyTest() throws InterruptedException {
+    void shouldShowErrorIfPhoneEmptyTest() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Красовский Алексей");
         form.$("[data-test-id=phone] input").setValue("");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-        Thread.sleep(3000);
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
     }
 
     @Test
-    void shouldShowErrorIfNameInvalid() throws InterruptedException {
+    void shouldShowErrorIfNameInvalid() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Krasovskiy");
         form.$("[data-test-id=phone] input").setValue("+79139999999");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-        Thread.sleep(3000);
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
-    void shouldShowErrorIfPhoneInvalid() throws InterruptedException {
+    void shouldShowErrorIfPhoneInvalid() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Красовский Алексей");
         form.$("[data-test-id=phone] input").setValue("+7913999");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-        Thread.sleep(3000);
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 }
 
