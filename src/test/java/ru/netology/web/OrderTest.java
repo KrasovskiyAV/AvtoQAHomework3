cda0891ac190dde28a8beec741ae14a179c0e2fe
Красовский АВ <AV.Krasovskiy@ya.ru>
2023-07-04 14:59:03 +0700

    app-order.jar added

---
+++ src/test/java/ru/netology/web/OrderTest.java
@@ -0,0 +1,23 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class OrderTest {
+    @Test
+    void shouldTest() throws InterruptedException {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Красовский Алексей");
+        form.$("[data-test-id=phone] input").setValue("+79139999999");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        Thread.sleep(5000);
+    }
+}
+
